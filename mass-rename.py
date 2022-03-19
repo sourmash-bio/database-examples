600f3db12a10daabbc063f3686f36bd337cb086a
C. Titus Brown <titus@idyll.org>
2022-03-19 08:28:25 -0700

    add better notify

--- mass-rename.py
+++ mass-rename.py
@@ -80,13 +80,15 @@ def massrename(args):
         error(f"Here are some examples: {example_missing}")
         sys.exit(-1)
 
+    notify("Everything looks copacetic. Proceeding to rename!")
+
     # go through, do rename, save.
     with sourmash_args.SaveSignaturesToLocation(args.output) as save_sigs:
         n = 0
         for idx in idx_list:
             for ss in idx.signatures():
                 n += 1
-                if n % 1000 == 0:
+                if n % 100 == 0:
                     notify(f"...at signature {n}", end="\r")
                 ident = ss.name.split(' ')[0]
                 new_name = rename_d[ident]
