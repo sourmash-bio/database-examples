96fb6888552d4ce0adc5e0b31656301475ca2bba
C. Titus Brown <titus@idyll.org>
2022-03-19 08:24:46 -0700

    upd

--- mass-rename.py
+++ mass-rename.py
@@ -2,7 +2,7 @@
 """
 Rename signatures in bulk.
 
-mass-rename.py takes a list of databases along with a spreadsheet (w/-F),
+mass-rename.py takes a list of databases along with spreadsheets (w/-F),
 and renames all of the matching signatures in the databases.
 
 The spreadsheet must contain two columns, 'ident' and 'name'; signatures
@@ -29,26 +29,28 @@ def massrename(args):
     moltype = sourmash_args.calculate_moltype(args)
     #CTB _extend_signatures_with_from_file(args)
 
-    # load spreadsheet
+    # load spreadsheets
     rename_d = {}
-    with open(args.from_spreadsheet, newline='') as fp:
-        r = csv.DictReader(fp)
-        for row in r:
-            name = row['name']
-            ident = row['ident']
+    for filename in args.from_spreadsheet:
+        count = 0
+        with open(filename, newline='') as fp:
+            r = csv.DictReader(fp)
+            for row in r:
+                name = row['name']
+                ident = row['ident']
 
-            assert ' ' not in ident, "identifiers cannot have spaces"
-            assert ident not in rename_d, "duplicates not allowed"
-            rename_d[ident] = name
-    notify(f"loaded {len(rename_d)} identifiers w/new names from '{args.from_spreadsheet}'")
+                assert ' ' not in ident, f"identifiers cannot have spaces - but '{ident}' does."
+                assert ident not in rename_d, f"duplicate identifer: '{ident}'"
+                rename_d[ident] = name
+                count += 1
+        notify(f"loaded {count} identifiers w/new names from '{filename}'")
+
+    notify(f"loaded a total of {len(rename_d)} distinct identifiers.")
 
     rename_set = set(rename_d)
 
     # build a new picklist for just the idents
-    if args.strip_identifier_versions:
-        ident_picklist = SignaturePicklist('identprefix')
-    else:
-        ident_picklist = SignaturePicklist('ident')
+    ident_picklist = SignaturePicklist('ident')
     ident_picklist.pickset = rename_set
 
     # go through all the database and load etc.
@@ -74,6 +76,8 @@ def massrename(args):
     if not to_rename.issubset(ident_picklist.found):
         remaining = to_rename - ident_picklist.found
         error(f"ERROR: {len(remaining)} identifiers from spreadsheet not found.")
+        example_missing = "\n".join(remaining)
+        error(f"Here are some examples: {example_missing}")
         sys.exit(-1)
 
     # go through, do rename, save.
@@ -105,11 +109,9 @@ def main():
         '-f', '--force', action='store_true',
         help='try to load all files as signatures'
     )
-    p.add_argument('--strip-identifier-versions',
-                   help='use only the identifier prefix (w/o version) for matching',
-                   action='store_true')
     p.add_argument('-F', '--from-spreadsheet',
                    required=True,
+                   action='append', default=[],
                    help="input spreadsheet containing 'ident' and 'name' columns")
 
     add_ksize_arg(p, 31)
