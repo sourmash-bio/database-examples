9200f3dee144299a7da32e184b5b7fb9bfcca049
C. Titus Brown <titus@idyll.org>
2022-04-10 08:09:50 -0700

    support new sqlite3 manifests from sourmash-bio/sourmash#1933

--- sigs-to-manifest.py
+++ sigs-to-manifest.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 import sys
 import sourmash
-from sourmash.logging import notify, error
+from sourmash.logging import notify, error, set_quiet
 import argparse
 from sourmash.manifest import CollectionManifest
 
@@ -11,16 +11,31 @@ def main():
     p.add_argument('pathlist', nargs='+')
     p.add_argument('-o', '--output', help='manifest output file',
                    required=True)
+    p.add_argument('-F', '--database-format', help='manifest output format',
+                   default='sql',
+                   choices=['csv', 'sql'])
     p.add_argument('--previous', help='previous manifest file')
     p.add_argument('--merge-previous', action='store_true',
                    help='merge previous and new manifests')
+    p.add_argument('-d', '--debug', action='store_true',
+                   help='output sourmash debug messages')
     args = p.parse_args()
+
+    set_quiet(False, args.debug)
+
+    if args.previous and args.previous == args.output:
+        if not args.merge_previous:
+            error("ERROR: --output and --previous are the same, but --merge-previous not specified.")
+            error("ERROR: so --previous would be overwritten with only new entries!?")
+            error("ERROR: I'm worried this doesn't make sense, so I'm exiting. Good bye!")
+            sys.exit(-1)
 
     previous_filenames = set()
     previous = CollectionManifest([])
     if args.previous:
         notify(f"loading previous manifest from '{args.previous}'")
         previous = CollectionManifest.load_from_filename(args.previous)
+        assert previous is not None
 
         for row in previous.rows:
             previous_filenames.add(row['internal_location'])
@@ -55,12 +70,16 @@ def main():
 
         notify(f"Loaded {n_loaded} manifest rows from files in '{filename}'")
 
+    if not rows:
+        notify(f"NO NEW MANIFEST ROWS DETECTED. Exiting!")
+        return 0
+
     if args.merge_previous:
         notify(f"merging {len(previous.rows)} previous rows into current.")
         rows.extend(previous.rows)
 
     m = CollectionManifest(rows)
-    m.write_to_filename(args.output)
+    m.write_to_filename(args.output, database_format=args.database_format)
 
     notify(f"saved {len(m)} manifest rows to '{args.output}'")
 
